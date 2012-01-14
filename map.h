069430a15564d4776099ea5e6df020720935913c
Miks Kalnins <MiksKalnins@MaikuMori.com>
2012-01-14 21:37:38 +0200

    Nothing here.

--- map.h
+++ map.h
@@ -8,9 +8,22 @@
 struct tank_map {
     char * data;
     int mapId;
-    int maxPlayers;
+    int maxPlayers; // citur?
     int size_x;
     int size_y;
     struct tank_map *next;
     struct tank_map *prev;
-}typedef tank_map;
+} typedef tank_map;
+
+enum map_tile {
+    WALL,
+    TANK_UP,
+    TANK_DOWN,
+    TANK_LEFT,
+    TANK_RIGHT
+};
+
+void map_init(int map_size_x, int map_size_y);
+void map_set(int x, int y, enum map_tile);
+void map_draw(struct map * m);
+void map_free(void);
