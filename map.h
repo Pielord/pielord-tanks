8152a7d00972d0fea37253ad6ca2a377932817fe
Rihards Balašs <rihardiii@gmail.com>
2012-01-14 19:41:43 +0200

    progress 0.0000000001

--- map.h
+++ map.h
@@ -1,9 +1,9 @@
-#define MAP_WALL 124
-#define TANK_SHOT 421
-#define TANK_UP 944
-#define TANK_DOWN 118
-#define TANK_LEFT 608
-#define TANK_RIGHT 628
+#define MAP_WALL 124	// |
+#define TANK_SHOT 421	// *
+#define TANK_UP 944		// ^
+#define TANK_DOWN 118	// v
+#define TANK_LEFT 608	// <
+#define TANK_RIGHT 628	// >
 
 struct map {
     char * data;
