affa6a179df96d25571c1ffd9da69828a335f252
Mārtiņš Balodis <martins256@gmail.com>
2012-01-14 19:12:52 +0200

    Added: add_player function

--- player.c
+++ player.c
@@ -1,4 +1,26 @@
-struct player * all_players;
+static struct player * all_players;
+static int player_count = 0;
+
+/**
+ * Create new player.
+ * @param id
+ * @param x
+ * @param y
+ */
+void add_player(int id, int x, int y) {
+    
+    all_players++;
+    
+    // add place for player in memory
+    all_players = realloc (all_players, sizeof(struct player) * player_count);
+    
+    
+    // set player x,y coords
+    all_players[all_players-1]->x = (float) x;
+    all_players[all_players-1]->y = (float) y;
+    
+}
+
 
 void player_init_players(int player_count)
 {
